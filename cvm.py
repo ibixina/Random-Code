c8b27e6e79b56c50bb8823814831700d709db9c7
Shishir <theshishir01@gmail.com>
2024-10-09 19:32:16 -0500

    comments

--- cvm.py
+++ cvm.py
@@ -1,30 +1,31 @@
 import random
 def main():
-    t = -1
-    p = 1
-    b = set()
+    t = -1 # timestep
+    p = 1 # probability of any element being in the buffer
+    b = set() # buffer
 
-    b_size = 20
-    m = 150
+    b_size = 20 # buffer size
+    m = 150 # length of the stream
 
-    data_stream = [random.randint(0,m*2) for i in range(m)]
+    data_stream = [random.randint(0,m*2) for i in range(m)] # random stream of data of length m
     print(len(data_stream))
-    answer = set(data_stream)
-    answer_length = len(answer)
+    answer = set(data_stream) # distinct elements in the stream
+    answer_length = len(answer) # number of distinct elements
     print("Answer: ", answer_length)
     while t != m-1:
         t += 1
-        a = data_stream[t]
-        max_el = (0,-1)
-        to_delete = []
+        a = data_stream[t] # get the next element from the stream
+        max_el = (0,-1) # keep track of the maximum u for later operation
+        to_delete = [] # keep track of items to be deleted, can't delete while the set is being iterated over
         for b_prime, u in b:
             if (u > max_el[1]):
                 max_el = (b_prime, u)
             if b_prime == a:
-                to_delete += [(b_prime, u)]
-        for delement in to_delete:
+                to_delete += [(b_prime, u)] # add it to be deleted once the loop is over
+        for delement in to_delete: # delete items
             b.remove(delement)
-        u = random.random()
+
+        u = random.random() # random value between 0 and 1
         if (u >= p):
             continue
         if (len(b) < b_size):
