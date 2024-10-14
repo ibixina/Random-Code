a549e35c692f302e1efa962a3ce36c242aba5f0e
Shishir <theshishir01@gmail.com>
2024-10-14 16:51:08 -0500

    a

---
+++ packer2.py
@@ -0,0 +1,13 @@
+c = """
+I=input
+u = set()
+n = int(I())
+for i in range(n):
+    for c in I():
+        if c.lower() in "abcdefghijklmnopqrstuvwxyz":u.add(c)
+print(f'{["not ",""][n==len(u)]}Cramer')
+x=list(u)
+x.sort()
+if x:print("".join(x))
+"""
+print(c.encode().decode('u16'))
